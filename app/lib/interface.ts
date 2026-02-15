fa8a9484b4f4d7aa7ff089d64cec94042ab93607
cdricn <cdricncdricn@gmail.com>
2026-02-15 22:30:57 +0800

    Modified interface to handle strings temporarily

--- app/lib/interface.ts
+++ app/lib/interface.ts
@@ -1,4 +1,4 @@
-export interface calendar {
+export interface CalendarData {
   month: string,
   day: number,
   day_code: number,
@@ -6,10 +6,14 @@ export interface calendar {
   night_weather: string | null,
   special_day_weather: string | null,
   special_night_weather: string | null,
-  city_events: Array<string> | null,
-  confidant_events: Array<string> | null,
-  events: Array<string> | null,
-  events_spoiler: Array<string> | null,
-  day_activities: string,
-  night_activities: string
+  city_events: Array<string> | null| string,
+  confidant_events: Array<string> | null| string,
+  events: Array<string> | null | string,
+  events_spoiler: Array<string> | null | string,
+  day_activities: string | null,
+  night_activities: string | null
+}
+
+export interface Calendar {
+  [month: string] : Array<CalendarData>;
 }
