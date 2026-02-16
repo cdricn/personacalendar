d296c56090735a3a8f4893ba8f758a716847b1f9
cdricn <cdricncdricn@gmail.com>
2026-02-16 23:38:01 +0800

    Added general layout

--- app/[id]/page.tsx
+++ app/[id]/page.tsx
@@ -1,40 +1,51 @@
 'use client';
 
 import styles from "./page.module.css";
-import useSWR from 'swr'
+import useSWR from 'swr';
 import getData from "../utils/getData";
 import { usePathname } from "next/navigation";
 import { useState } from "react";
 import Months from "./calendar/months";
 import Days from "./calendar/days";
+import Schedule from "./schedule/schedule";
 
 export default function CalendarPage() {
   const path = usePathname();
-  const { data, error, isLoading } = useSWR(`${path}`, getData)
+  const { data, error, isLoading } = useSWR(`${path}`, getData);
   const [currentMonth, setCurrentMonth] = useState('april');
+  const [currentDay, setCurrentDay] = useState(0);
 
   if (isLoading) return <>Loading</>;
   if (error) return <>Error</>;
 
   function setMonth(clickedMonth:string) {
     setCurrentMonth(clickedMonth);
-    
   }
 
-  console.log(data)
+  function setDay(clickedDay:number) {
+    setCurrentDay(clickedDay);
+  }
 
   return (
     <main>
       <h1>Calendar</h1>
-      <Months setMonth={setMonth}/>
-      {!isLoading && data !== undefined ?
-        <>
-          <Days monthLength={data[currentMonth].length}/>
-          <div>
-          </div>
-        </> :
-        <>err</>
-      }
+      <section className={styles['main-content']}>
+        {!isLoading && data !== undefined ?
+          <>
+            <div className={styles['calendar-container']}>
+              <Months setMonth={setMonth}/>
+              <Days monthLength={data[currentMonth].length} 
+                monthStart={data[currentMonth][0].day_code}
+                setDay={setDay}/>
+            </div>
+            <div className={styles['schedule-container']}>
+              <Schedule scheduleData={data[currentMonth][currentDay]}/>
+            </div>
+          </>
+          :
+          <>err</>
+        }
+      </section>
     </main>
   )
 }
