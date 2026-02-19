ab0b270087ccd6efd1aa0a28c7c64447dd13ce9d
cdricn <cdricncdricn@gmail.com>
2026-02-19 23:30:36 +0800

    Added useEffect to return set page font

--- app/[id]/page.tsx
+++ app/[id]/page.tsx
@@ -4,7 +4,7 @@ import styles from "./page.module.css";
 import useSWR from 'swr';
 import getData from "../utils/getData";
 import { usePathname } from "next/navigation";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Months from "./calendar/months";
 import Days from "./calendar/days";
 import Schedule from "./schedule/schedule";
@@ -14,6 +14,14 @@ export default function CalendarPage() {
   const { data, error, isLoading } = useSWR(`${path}`, getData);
   const [currentMonth, setCurrentMonth] = useState('april');
   const [currentDay, setCurrentDay] = useState(0);
+
+  useEffect(()=>{
+    const theme = path.slice(1);
+    document.querySelector('body')?.setAttribute('data-theme', theme);
+    return () => {
+      document.body.removeAttribute('data-theme');
+    }
+  }, [])
 
   if (isLoading) return <>Loading</>;
   if (error) return <>Error</>;
@@ -26,8 +34,10 @@ export default function CalendarPage() {
     setCurrentDay(clickedDay);
   }
 
+  
+
   return (
-    <main>
+    <main className={styles['main']}>
       <h1>Calendar</h1>
       <section className={styles['main-content']}>
         {!isLoading && data !== undefined ?
