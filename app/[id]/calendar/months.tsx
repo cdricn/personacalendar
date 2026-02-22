5edf04dcebd26bc8359ff8b4e864ede0c8589a4c
cdricn <cdricncdricn@gmail.com>
2026-02-22 13:18:45 +0800

    Changed function and layout

--- app/[id]/calendar/months.tsx
+++ app/[id]/calendar/months.tsx
@@ -1,38 +1,41 @@
 'use client';
 
 import styles from './calendar.module.css';
+import { useEffect, useState } from 'react';
 
-export default function Months({setMonth}:{setMonth:(item:string)=>void}) {
+export default function Months({setMonth}:{setMonth:(item:string)=>void,}) {
+  const [monthIndex, setMonthIndex] = useState(0);
+  const months = [
+    'april', 'may', 'june', 'july', 'august', 'september',
+    'october', 'november', 'december', 'january', 'february', 'march'
+  ]
 
-  function handleClick(month:string) {
-    setMonth(month);
+  useEffect(()=>{
+    setMonth(months[monthIndex]);
+  }, [monthIndex])
+
+  function handleClick(action:string) {
+    if(action == 'prev' && monthIndex > 0) {
+      setMonthIndex(monthIndex-1);
+    }
+    if(action == 'next' && monthIndex < 11) {
+      setMonthIndex(monthIndex+1);
+    }
   }
-
-  const months = {
-    april: 'APR',
-    may: 'MAY',
-    june: 'JUN',
-    july: 'JUL',
-    august: 'AUG',
-    september: 'SEP',
-    october: 'OCT',
-    november: 'NOV',
-    december: 'DEC',
-    january: 'JAN',
-    february: 'FEB',
-    march: 'MAR'
-  };
 
   return (
     <div className={styles['months-container']}>
-      {Object.entries(months).map((item)=>{
-        const [key, value] = item;
-        return (
-          <div key={key} className={styles['month']} onClick={()=>handleClick(key)}>
-            <span>{value}</span>
-          </div>
-        );
-      })}
+      <div className={styles['month-nav']}
+        onClick={()=>handleClick('prev')}>
+          {'<'}
+      </div>
+      <span className={styles['month']}>
+        {months[monthIndex].slice(0, 1).toUpperCase() + months[monthIndex].slice(1)} 20XX
+      </span>
+      <div className={styles['month-nav']}
+        onClick={()=>handleClick('next')}>
+          {'>'}
+      </div>
     </div>
   )
 }
