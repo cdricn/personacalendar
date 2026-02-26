e6983c70e94cd7c16c2ca46de1d8b39485cd53a4
cdricn <cdricncdricn@gmail.com>
2026-02-26 20:21:43 +0800

    Removed additional component for landing

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,20 @@
-import Landing from "./landing/landing";
+import Link from "next/link";
+import Games from "./data/games.json"
 
 export default function Main() {
   return (
     <main>
-      <Landing />
+      <h1>Calendar</h1>
+      <p>Some description about the website</p>
+      <section>
+        {Games.map((item)=>{
+          return (
+            <div key={item.id}>
+              <Link href={item.link}>{item.link}</Link>
+            </div>
+          )
+        })}
+      </section>
     </main>
   );
 }
