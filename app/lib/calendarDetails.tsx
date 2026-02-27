649a4372a99bccbc9ac90776d2dfa5fe8b862639
cdricn <cdricncdricn@gmail.com>
2026-02-27 20:04:31 +0800

    Renamed file

---
+++ app/lib/calendarDetails.tsx
@@ -0,0 +1,11 @@
+// Planning on making these passed like so for other games 
+// where months/days may have a different structure
+
+export const personaMonths = [
+  'april', 'may', 'june', 'july', 'august', 'september',
+  'october', 'november', 'december', 'january', 'february', 'march'
+];
+
+export const personaDays = [
+  'Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'
+];
