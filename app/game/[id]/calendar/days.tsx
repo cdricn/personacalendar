890dba6012164ea311c91c93ef213dd287f6ecd0
cdricn <cdricncdricn@gmail.com>
2026-02-27 18:05:20 +0800

    Made more reusable by separating content icons

--- app/game/[id]/calendar/days.tsx
+++ app/game/[id]/calendar/days.tsx
@@ -1,50 +1,42 @@
-import CalendarBlock from './calendarBlock';
+
 import styles from './days.module.css';
-import { CalendarData } from '@/app/lib/interface';
+import { DataContext } from '@/app/utils/context';
+import { use } from 'react';
+import CalendarBlock from './calendarBlock';
 // icons used: FaExclamation, FaGlobeAsia, GiCardAceHearts ri/fa, ri/gi
 
 export default function Days({
-  days, setDay, maxRows, data
+  days, setDay, maxRows
 }:{
-  days: string[], setDay:(item:number)=>void, maxRows:number, data: CalendarData[]
+  days: string[], setDay:(item:number)=>void, maxRows:number
 }) {
-  
+
+  const data = use(DataContext);
+
+  if (!data) return <></>;
   const monthStart = data[0].day_code;
 
   function handleClick(currentDay:number) {
     setDay(currentDay);
   }
 
-  function calendarHint(src:string, clamp:string) {
-    return (
-      <img className={styles['hint-icon']} src={src} 
-        style={{
-          width: clamp
-        }}/>
-    )
-  }
-  
-  const confidantIconClamp = `clamp(15px, calc(0.9375rem + ((1vw - 2.8px) * 2.7439)), 60px)`;
-  const alertIconClamp = `clamp(8px, calc(0.5rem + ((1vw - 2.8px) * 1.3415)), 30px)`;
-
   function populateDays() {
     let daysArray = [];
+    
+    if (!data) return;
+
     for(let i=0; i<data.length; i++) {
       const dayKey = 'day' + (i+1);
-      const {day_activities, night_activities, events, confidant_events, activities} = data[i];
 
       daysArray.push(
         <div key={dayKey} className={styles['number']} 
-          onClick={()=>{handleClick(i)}}
-          style={i==0?{gridColumnStart: `${monthStart}`}:undefined}>
-            <span>{i+1}</span>
-            <CalendarBlock />
-            
-            <div className={styles['hint-icons-container']}>
-              {confidant_events===null?null: calendarHint('/confidant.png', confidantIconClamp)}
-              {activities===null?null: calendarHint('/alert.png', alertIconClamp)}
-              {events===null?null: calendarHint('/alert.png', alertIconClamp)}
-            </div>
+          onClick={data[i].invalidDay ? undefined : ()=>{handleClick(i)}}
+          style={i==0 ? {gridColumnStart: `${monthStart}`} : undefined}
+          >
+            <span className={data[i].invalidDay ? styles['inactive'] : undefined}>
+              {i+1}
+            </span>
+            <CalendarBlock day={i}/>
         </div>
       );
     }
