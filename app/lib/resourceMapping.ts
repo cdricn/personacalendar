3e1317ea0677a780a376f4b1e08d9af544a85dd5
cdricn <cdricncdricn@gmail.com>
2026-06-28 02:03:35 +0800

    Replace png weather icons with svgs

--- app/lib/resourceMapping.ts
+++ app/lib/resourceMapping.ts
@@ -10,10 +10,10 @@ const normalDays = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'] as const;
 export const ResourceMapping : GameIcons = {
   ['/persona5_royal']: {
     day_modifier: {
-      clear: {src: '/P5_Weather_Clear.webp', alt:'Clear weather icon'},
-      cloudy: {src: '/P5_Weather_Cloudy.webp', alt:'Cloudy weather icon'},
-      rainy: {src: '/P5_Weather_Rainy.webp', alt:'Rainy weather icon'},
-      snowy: {src: '/P5_Weather_Snowy.webp', alt:'Snowy weather icon'},
+      clear: {src: '/P5_Weather_Clear.svg', alt:'Clear weather icon'},
+      cloudy: {src: '/P5_Weather_Cloudy.svg', alt:'Cloudy weather icon'},
+      rainy: {src: '/P5_Weather_Rainy.svg', alt:'Rainy weather icon'},
+      snowy: {src: '/P5_Weather_Snowy.svg', alt:'Snowy weather icon'},
     },
     special_day_modifier: {},
     monthHeaders: gregorianMonths,
