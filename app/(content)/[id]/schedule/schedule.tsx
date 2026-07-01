c629448a7fe3622fd3e51737f003303adfedefe3
cdricn <cdricncdricn@gmail.com>
2026-07-01 20:08:41 +0800

    Update schedule layout

--- app/(content)/[id]/schedule/schedule.tsx
+++ app/(content)/[id]/schedule/schedule.tsx
@@ -1,19 +1,19 @@
 'use client';
 
 import styles from './schedule.module.css';
-import { DataContext } from '@/app/utils/context';
+import { GameContext, DataContext } from '@/app/utils/context';
 import { use } from 'react';
-import Weather from '../../../components/weather';
-import InfoBlob from '../../../components/infoBlob';
-import Confidants from '../../../components/confidants';
+import { ResourceMapping } from '@/app/lib/resourceMapping';
 
 export default function Schedule({currentDay}:{currentDay:number}) {
+  const game = use(GameContext);
   const data = use(DataContext);
   if (!data) return <></>;
   
+  const {dayHeaders} = ResourceMapping[game];
   const scheduleData = data[currentDay];
   const { 
-    day, day_weather, night_weather, special_day_weather, special_night_weather,
+    day, day_code, day_weather, night_weather, special_day_weather, special_night_weather,
     world, activities, social_events, events, events_spoiler, day_activities, night_activities    
   } = scheduleData;
 
@@ -32,41 +32,28 @@ export default function Schedule({currentDay}:{currentDay:number}) {
     december: '12',
   };
 
+  console.log(scheduleData)
+
   return (
     <>
       <div className={styles['section-header']}>
-        <h2></h2>
-        <div className={styles['date']}>
-          <span>month</span>
-          <span>/</span>
-          <span>{day}</span>
-        </div>
+        <h2>{day}/{'9'} {dayHeaders[day_code]}</h2>
       </div>
 
       <div className={styles['day-modifiers']}>
         <div className={styles['modifier']}>
-          <span>{day_activities ? day_activities : 'N/A'}</span>
+          <span>{day_activities}</span>
         </div>
         <div className={styles['modifier']}>
-          <span>{night_activities ? night_activities : 'N/A'}</span>
+          <span>{night_activities}</span>
         </div>
       </div>
 
       <div className={styles['schedule']}>
-        <div className={styles['info-block']}>
-          <h3>Schedule</h3>
-        </div>
-        <div className={styles['info-block']}>
-          <h3>Confidant Events</h3>
-        </div>
-        <div className={styles['info-block']}>
-          <h3>Confidants</h3>
-          <Confidants currentDay={currentDay}/>
-        </div>
-        <div className={styles['info-block']}>
-          <h3>Activities</h3>
-        </div>
-        <p>{world}</p>
+        <h3>Day</h3>
+        <h3>Night</h3>
+        <h3>Events</h3>
+        <h3>Story</h3>
       </div>
     </>
   )
