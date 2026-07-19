4867496725e006ddfd0039b9b4e9d0e97913f45f
cdricn <cdricncdricn@gmail.com>
2026-07-19 17:09:39 +0800

    Fix interface to match new confidants structure

--- app/lib/interface.ts
+++ app/lib/interface.ts
@@ -5,11 +5,11 @@ export interface CalendarDays {
   night_weather:          string | null,
   special_day_weather:    string | null,
   special_night_weather:  string | null,
-  world:                  string | string[] | null,
-  activities:             string | string[] | null,
-  social_events:          string | string[] | null,
-  events:                 string | string[] | null,
-  events_spoiler:         string | string[] | null,
+  world:                  string[] | null,
+  activities:             string[] | null,
+  social_events:          string[] | null,
+  events:                 string[] | null,
+  events_spoiler:         string[] | null,
   day_activities:         string | null,
   night_activities:       string | null,
   is_day_playable:        boolean
@@ -31,22 +31,38 @@ interface IconMap {
   },
   monthHeaders: readonly string[],
   dayHeaders: readonly string[],
-  confidants: readonly ConfidantData[]
+  confidants: ConfidantData
 }
 export interface GameIcons {
   [game:string]: IconMap
 }
 
 export interface ConfidantData {
+  normal_arcanas: NormalArcanas[],
+  special_arcanas: SpecialArcanas[]
+}
+
+export interface NormalArcanas {
   name: string,
   short_name: string,
   arcana: string,
-  unlock: {type: string, month: string, day: number},
-  condition: string | null
+  unlock_date: string,
+  rank_up: {
+    initiate: string,
+    conditions: string[]
+  },
   schedule: {
     ignoresRain: boolean,
     time: string,
     availability: {[day:string]:string | undefined}
-  } | null
+  } | null,
+  location: string[]
 }
 
+export interface SpecialArcanas {
+  name: string,
+  short_name: string,
+  arcana: string,
+  unlock_date: string,
+  condition: string
+}
