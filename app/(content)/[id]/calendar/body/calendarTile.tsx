e0bda0dca4e1dfdbb48a5ba42d3ece56169d1c71
cdricn <cdricncdricn@gmail.com>
2026-08-12 17:42:17 +0800

    Move files inside the dynamic folder

---
+++ app/(content)/[id]/calendar/body/calendarTile.tsx
@@ -0,0 +1,46 @@
+import styles from './calendarTile.module.css';
+import { CalendarDays } from "../../../../lib/interface";
+import { Stripes } from '../../../../components/svgItems';
+
+interface day_modifier {
+  [modifier: string]: {
+    src: string;
+    alt: string;
+  };
+}
+
+export default function calendarTile({
+  item, 
+  day_modifier,
+  isSelected
+}:{
+  item: CalendarDays, 
+  day_modifier: day_modifier,
+  isSelected: number | null
+}) {
+
+  const isTileSelected = isSelected === item.day;
+  const weatherAlign = item.day_weather && item.night_weather ? 'double' : 'single';
+  const tileColor = !item.is_day_playable ? undefined :
+    item.day_activities === 'Unavailable' && item.night_activities === 'Unavailable' ? 'alert' : 
+    item.day_activities === 'Limited' || item.night_activities === 'Limited' ? 'warning' :
+    item.activities || item.social_events ? 'event' : 
+    'normal';
+
+  return (
+    <div id={item.day.toString()} className={styles['day-tile-container']}>
+      <div className={styles['tile']} 
+        data-clickable={item.is_day_playable}
+        data-background={tileColor}
+        data-selected={isTileSelected}
+      >
+        {!item.is_day_playable ? <div className={styles['stripes']}><Stripes/></div> : null}
+        <div className={styles['weather-icon-container']} data-icon-alignment={weatherAlign}>
+          {item.day_weather ? <img src={day_modifier[item.day_weather].src} alt={day_modifier[item.day_weather].alt} /> : null}
+          {item.night_weather ? <img src={day_modifier[item.night_weather].src} alt={day_modifier[item.night_weather].alt} /> : null}
+        </div>
+        <span>{item.day}</span>
+      </div>
+    </div>
+  )
+}
