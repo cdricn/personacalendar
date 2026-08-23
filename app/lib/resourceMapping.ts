4d3e187f179b740af02eca786e3f300db214db58
cdricn <cdricncdricn@gmail.com>
2026-08-23 14:55:51 +0800

    Add placeholder data for other games

--- app/lib/resourceMapping.ts
+++ app/lib/resourceMapping.ts
@@ -5,8 +5,6 @@ const gregorianMonths = [
   'April', 'May', 'June', 'July', 'August', 'September',
   'October', 'November', 'December', 'January', 'February', 'March'
 ] as const;
-const normalDays = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'] as const;
-
 export const gregorianMonthsToDigit : {[month:string]: string} = {
   january: '1',
   february: '2',
@@ -21,9 +19,36 @@ export const gregorianMonthsToDigit : {[month:string]: string} = {
   november: '11',
   december: '12',
 };
+const normalDays = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'] as const;
 
 export const ResourceMapping : GameIcons = {
   ['/persona5_royal']: {
+    day_modifier: {
+      clear: {src: '/P5_Weather_Clear.svg', alt:'Clear weather icon'},
+      cloudy: {src: '/P5_Weather_Cloudy.svg', alt:'Cloudy weather icon'},
+      rainy: {src: '/P5_Weather_Rainy.svg', alt:'Rainy weather icon'},
+      snowy: {src: '/P5_Weather_Snowy.svg', alt:'Snowy weather icon'},
+    },
+    special_day_modifier: {},
+    monthHeaders: gregorianMonths,
+    dayHeaders: normalDays,
+    confidants: p5royal_confidants
+  },
+  // PLACEHOLDERS
+  // CHANGE TO RESPECTIVE GAMES LATER
+  ['/persona4_golden']: {
+    day_modifier: {
+      clear: {src: '/P5_Weather_Clear.svg', alt:'Clear weather icon'},
+      cloudy: {src: '/P5_Weather_Cloudy.svg', alt:'Cloudy weather icon'},
+      rainy: {src: '/P5_Weather_Rainy.svg', alt:'Rainy weather icon'},
+      snowy: {src: '/P5_Weather_Snowy.svg', alt:'Snowy weather icon'},
+    },
+    special_day_modifier: {},
+    monthHeaders: gregorianMonths,
+    dayHeaders: normalDays,
+    confidants: p5royal_confidants
+  },
+  ['/persona3_reload']: {
     day_modifier: {
       clear: {src: '/P5_Weather_Clear.svg', alt:'Clear weather icon'},
       cloudy: {src: '/P5_Weather_Cloudy.svg', alt:'Cloudy weather icon'},
