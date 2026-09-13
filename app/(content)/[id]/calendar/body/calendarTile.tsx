45ace637d496557b1bccd274db41018e401d05b1
cdricn <cdricncdricn@gmail.com>
2026-09-13 23:46:25 +0800

    refactor: add function for tile weather icons
    Change from ternary to if/else because it's more
    readable; also removes duplicated ternary for day
    and night weather icons

--- app/(content)/[id]/calendar/body/calendarTile.tsx
+++ app/(content)/[id]/calendar/body/calendarTile.tsx
@@ -1,32 +1,40 @@
 import styles from './calendarTile.module.css';
-import { CalendarDays } from "../../../../lib/interface";
+import { Image, CalendarDays } from "../../../../lib/interface";
 import { Stripes } from '../../../../components/svgItems';
-
-interface day_modifier {
-  [modifier: string]: {
-    src: string;
-    alt: string;
-  };
-}
 
 export default function calendarTile({
   item, 
-  day_modifier,
-  isSelected
+  weather_icons,
+  isSelected,
+  general_icons
 }:{
   item: CalendarDays, 
-  day_modifier: day_modifier,
-  isSelected: number | null
+  weather_icons: Image,
+  isSelected: number | null,
+  general_icons: Image
 }) {
 
   const isTileSelected = isSelected === item.day;
-  const weatherAlign = item.day_weather && item.night_weather ? 'double' : 'single';
-  const tileColor = !item.is_day_playable ? undefined :
-    (item.time_day === 'Unavailable' || item.time_night === 'Unavailable') ? 'alert' : 
-    (item.time_day === 'Limited' || item.time_night === 'Limited') ? 'warning' :
-    ((item.day_activities && item.night_activities) || item.social_events) ? 'event' : 
-    'normal';
   const hasNotice = item.notice ? 'hasNotice' : null;
+
+  const tileColor = !item.is_day_playable ? undefined :
+    item.notice ? 'alert' : 
+    // item.notice ? 'notice' - narrow notice further
+    item.social_events ? 'event' : 
+    'normal';
+
+  function addTileIcon(time: string | null, image_type: string | null) {
+    if (time === 'unavailable') {
+      return <img className={styles['unavailable_icon']} src={general_icons.unavailable.src} alt={general_icons.unavailable.alt} />;
+    } 
+    else if (time === 'limited') {
+      return <img className={styles['limited_icon']} src={general_icons.limited.src} alt={general_icons.limited.alt} />;
+    } 
+    else if (image_type) {
+      return <img src={weather_icons[image_type].src} alt={weather_icons[image_type].alt} />;
+    }
+    else return null;
+  }
 
   return (
     <div id={item.day.toString()} className={styles['tile']}>
@@ -37,9 +45,9 @@ export default function calendarTile({
         data-hasNotice={hasNotice}
       >
         {!item.is_day_playable ? <div className={styles['stripes']}><Stripes/></div> : null}
-        <div className={styles['weather-icon-container']} data-icon-alignment={weatherAlign}>
-          {item.day_weather ? <img src={day_modifier[item.day_weather].src} alt={day_modifier[item.day_weather].alt} /> : null}
-          {item.night_weather ? <img src={day_modifier[item.night_weather].src} alt={day_modifier[item.night_weather].alt} /> : null}
+        <div className={styles['weather-icon-container']} >
+          {addTileIcon(item.time_day, item.day_weather)}
+          {addTileIcon(item.time_night, item.night_weather)}
         </div>
         <span>{item.day}</span>
       </div>
